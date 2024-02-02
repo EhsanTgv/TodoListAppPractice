fd15737481b11b4b205c9fcbc695c7088f3d52cc
EhsanTgv <EhsanTgv@gmail.com>
2024-02-02 14:53:18 +0300

    Implement Task Cell

---
+++ TodoListAppPractice/TaskCell.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCell.swift
+//  TodoListAppPractice
+//
+//  Created by Ehsan Taghavi on 2.02.2024.
+//
+
+import SwiftUI
+
+struct TaskCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskCell()
+}
